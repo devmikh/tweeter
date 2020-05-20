8ff8f1617632375fcd1c81bd678776101e307878
Mikhail Serebriakov <mishacyb@gmail.com>
2020-05-20 20:27:22 +0000

    Implemented tweets fetching with AJAX

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,76 +4,66 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-const createTweetElement = (tweet) => {
-  return `<article class="tweet">
-          <header>
-            <div class='user'>
-              <div>
-                <img src="${tweet.user.avatars}">
-              </div>
-              <div>
-                <span>${tweet.user.name}</span>
-              </div>
-            </div>
-            <div class='username'>
-              <span>${tweet.user.handle}</span>
-            </div>
-          </header>
-          <main>
-            <p>${tweet.content.text}</p>
-          </main>
-          <footer>
-            <div class='date'>
-              <span>${tweet.created_at}</span>
-            </div>
-            <ul>
-              <li><a href="#"><i class="fas fa-flag"></i></a></li>
-              <li><a href="#"><i class="fas fa-retweet"></i></a></li>
-              <li><a href="#"><i class="fas fa-heart"></i></a></li>
-            </ul>
-          </footer>
-        </article>`
-};
 
-const renderTweets = (tweetArray) => {
-  for (const tweet of tweetArray) {
-    $("#tweet-container").append(createTweetElement(tweet));
-  }
-}
-
-const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    }
-  ]
 
 $(document).ready(function() {
-  renderTweets(data);
+  // Given a tweet object, creates and returns an HTML element in a string format
+  const createTweetElement = (tweet) => {
+    return `<article class="tweet">
+            <header>
+              <div class='user'>
+                <div>
+                  <img src="${tweet.user.avatars}">
+                </div>
+                <div>
+                  <span>${tweet.user.name}</span>
+                </div>
+              </div>
+              <div class='username'>
+                <span>${tweet.user.handle}</span>
+              </div>
+            </header>
+            <main>
+              <p>${tweet.content.text}</p>
+            </main>
+            <footer>
+              <div class='date'>
+                <span>${tweet.created_at}</span>
+              </div>
+              <ul>
+                <li><a href="#"><i class="fas fa-flag"></i></a></li>
+                <li><a href="#"><i class="fas fa-retweet"></i></a></li>
+                <li><a href="#"><i class="fas fa-heart"></i></a></li>
+              </ul>
+            </footer>
+          </article>`
+  };
+  
+  /* 
+  * Given an array of tweet objects, creates an HTML element
+  * for each of them and appends it to the #tweet-container
+  */
+  const renderTweets = (tweetArray) => {
+    for (const tweet of tweetArray) {
+      $("#tweet-container").prepend(createTweetElement(tweet));
+    }
+  }
+  
+  // Sends a GET request to the server and returns a JSON object
+  const loadTweets = () => {
+    $.getJSON('/tweets')
+      .then(function(tweets) {
+        renderTweets(tweets);
+      });
+  }
+
+  loadTweets();
 
   $('form').submit(function(event) {
     event.preventDefault();
 
     const data = $(this).serialize();
-    $.post('/tweets/', data)
+    $.post('/tweets', data)
       .then(function(response){
         console.log("response >>", response);
       }); 
