29a0ce9134b07cb824ae4d9d25ed402ca17a7d45
Mikhail Serebriakov <mishacyb@gmail.com>
2020-05-20 19:59:27 +0000

    Implemented loading tweets dynamically

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,68 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+const createTweetElement = (tweet) => {
+  return `<article class="tweet">
+          <header>
+            <div class='user'>
+              <div>
+                <img src="${tweet.user.avatars}">
+              </div>
+              <div>
+                <span>${tweet.user.name}</span>
+              </div>
+            </div>
+            <div class='username'>
+              <span>${tweet.user.handle}</span>
+            </div>
+          </header>
+          <main>
+            <p>${tweet.content.text}</p>
+          </main>
+          <footer>
+            <div class='date'>
+              <span>${tweet.created_at}</span>
+            </div>
+            <ul>
+              <li><a href="#"><i class="fas fa-flag"></i></a></li>
+              <li><a href="#"><i class="fas fa-retweet"></i></a></li>
+              <li><a href="#"><i class="fas fa-heart"></i></a></li>
+            </ul>
+          </footer>
+        </article>`
+};
+
+const renderTweets = (tweetArray) => {
+  for (const tweet of tweetArray) {
+    $("#tweet-container").append(createTweetElement(tweet));
+  }
+}
+
+const data = [
+    {
+      "user": {
+        "name": "Newton",
+        "avatars": "https://i.imgur.com/73hZDYK.png"
+        ,
+        "handle": "@SirIsaac"
+      },
+      "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants"
+      },
+      "created_at": 1461116232227
+    },
+    {
+      "user": {
+        "name": "Descartes",
+        "avatars": "https://i.imgur.com/nlhLi3I.png",
+        "handle": "@rd" },
+      "content": {
+        "text": "Je pense , donc je suis"
+      },
+      "created_at": 1461113959088
+    }
+  ]
+
+$(document).ready(function() {
+  renderTweets(data);
+});
